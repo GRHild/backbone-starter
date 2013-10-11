15720e2261596cb73773bd01c23c63a787883e49
Greg Hild <grhild@gmail.com>
2013-10-11 11:34:00 -0500

    Render template function to #store div.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 (function(){
 
-  var Cufflinks = new Product();
+  var cufflinks = new Product({
+  	name: "MadHatter Cufflinks",
+  	price: 100
+  });
 
+	var productView = new ProductView({
+		model: cufflinks,
+		el: '#store'
+	});
 
-
-
-
-
-
-
-
-
-
-
+	productView.render();
 })();
+
+
--- src/views/product-view.js
+++ src/views/product-view.js
@@ -1,8 +1,16 @@
 (function () {
+	// var productTemplateHtml = $('#templates .product').html();
+	// var productTemplate = _.template(productTemplateHtml);
+
+	var productTemplate = _.getTemplate('product');
+
 
   window.ProductView = Backbone.View.extend({
   	render: function() {
-  		$(this.el).html( this.model.get('name'));
+  		var newHtml = productTemplate( this.model.toJSON() );
+  		$(this.el).html(newHtml);
+
+  		// $(this.el).html( this.model.get('name'));
   	}
   });
 
